c422a06a14a0af81715863aa61b7412d185e1ae0
amirtnss <116731770+amirtnss@users.noreply.github.com>
2023-02-23 12:30:52 +0100

    Add files via upload

---
+++ body.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    
+<head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/body.css">
+        
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+        
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+<h1> body</h1>
+
+</body>
+</html>
